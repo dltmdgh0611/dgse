8d061f43aa9fd1843429ef79fe93681d710ffae8
stck622 <stck622@naver.com>
2019-05-15 22:32:06 +0900

    2

---
+++ dgse/dgse/VIEW/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dgse.VIEW
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int Number;
+
+        public int Number;
+
+    }
+}
